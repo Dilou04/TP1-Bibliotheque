364dd5865625fc15d02df361866309327489f8a1
Dilou04 <dylan.lit2004@gmail.com>
2025-12-04 10:58:57 +0100

    Ajout de la classe emprunt

--- emprunt.cpp
+++ emprunt.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "emprunt.h"
 #include "date.h"
 
@@ -6,10 +5,8 @@ std::string Emprunt::getDateEmprunt(){
     return dateEmprunt_;
 }
 
-=======
 #include "emprunt.h"
 
 std::string Emprunt::getDateEmprunt(){
     return dateEmprunt_;
 }
->>>>>>> 75cb30d78681b09d1d06b5ef149763d80fad36ee
--- emprunt.h
+++ emprunt.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef EMPRUNT_H
 #define EMPRUNT_H
 
@@ -12,19 +11,4 @@ class Emprunt{
         std::string dateEmprunt_;
 };
 
-=======
-#ifndef EMPRUNT_H
-#define EMPRUNT_H
-
-#include <iostream>
-#include <string>
-
-class Emprunt{
-    public:
-    std::string getDateEmprunt();
-    private:
-        std::string dateEmprunt_;
-};
-
->>>>>>> 75cb30d78681b09d1d06b5ef149763d80fad36ee
 #endif
